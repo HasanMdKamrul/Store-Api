5657c47cc9dd254aa54133cc4ac2f5f67f5cc590
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-02 03:45:26 +0200

    search result card added

--- js/shop.js
+++ js/shop.js
@@ -48,6 +48,40 @@ catagoryDisplay();
 
 document.getElementById('search-input').addEventListener('keypress', async(event)=>{
     if (event.key === 'Enter') {
+        const searchFieldValue = document.getElementById('search-input').value;
+        const searchResultContainer = document.getElementById('search-results');
+        searchResultContainer.textContent = ``;
+        // ** load data 
+        const allProducts = await loadAllProducts();
         
+        const filteredProducts = allProducts.filter(product => product.category.includes(searchFieldValue));
+
+
+        filteredProducts.length === 0 ? document.getElementById('no-data').classList.remove('d-none') : document.getElementById('no-data').classList.add('d-none')
+
+
+        console.log(filteredProducts)
+        
+        filteredProducts.forEach(product => {
+            const {category,title,image,price,description} = product;
+            const cardContent = document.createElement('div');
+            cardContent.classList.add('col',"my-5");
+            cardContent.innerHTML = `
+            <div class="card" style="height:800px">
+            <img src="${image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${category}</h5>
+                <h5 class="card-title">${title.length > 5 ? title.slice(0,5): title}</h5>
+                <p class="card-text">${description.length > 20 ? description.slice(0,20) : description }</p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Price: ${price}</small>
+            </div>
+            </div>
+            
+            `;
+
+            searchResultContainer.appendChild(cardContent);
+        })
     }
 })
