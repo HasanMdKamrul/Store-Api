a93153e3f3ade8d5802ef4d77b1e877cbfe0e492
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-02 04:07:18 +0200

    Single element detail display

--- js/shop.js
+++ js/shop.js
@@ -66,7 +66,7 @@ document.getElementById('search-input').addEventListener('keypress', async(event
         console.log(filteredProducts)
         
         filteredProducts.forEach(product => {
-            const {category,title,image,price,description} = product;
+            const {category,title,image,price,description,id} = product;
             const cardContent = document.createElement('div');
             cardContent.classList.add('col',"my-5");
             cardContent.innerHTML = `
@@ -79,6 +79,10 @@ document.getElementById('search-input').addEventListener('keypress', async(event
             </div>
             <div class="card-footer">
                 <small class="text-muted">Price: ${price}</small>
+                <button onclick='loadDetailsProduct(${id})' type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                     Product Details
+                </button>
+
             </div>
             </div>
             
@@ -98,6 +102,41 @@ document.getElementById('search-input').addEventListener('keypress', async(event
 const sppiner = (isSppiner)=>{
 
     isSppiner ? document.getElementById('sppiner').classList.remove('d-none') : document.getElementById('sppiner').classList.add('d-none')
+};
 
-    
+// ** loadProductdetails
+
+const loadDetailsProduct = async(productId)=>{
+
+    // ** product detail display
+    const modalBody = document.getElementById('modal-body')
+    // ** load all products data
+
+    console.log(productId)
+    const allProducts = await loadAllProducts();
+
+    const findProduct = allProducts.find(product => {
+        const {id} = product;
+        if (id === productId) {
+            const {category,description,image} = product;
+            return (category,image,description)
+        }
+        
+    });
+
+    const {image,description,category} = findProduct;
+
+    modalBody.innerHTML = `
+    <div class="card" style="width: 18rem;">
+    <img src="${image}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${category}</h5>
+        <p class="card-text">${description}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    </div>
+    `
+
+
+
 }
