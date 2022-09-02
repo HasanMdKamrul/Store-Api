334d5647b4a8d1c01edfb646d64a3923aeba93c3
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-02 03:11:29 +0200

    Catagory display added

--- js/shop.js
+++ js/shop.js
@@ -1,13 +1,53 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  </body>
-</html>
+// ** data loading
+
+const loadAllProducts = async()=>{
+    try {
+        const response = await fetch(`https://fakestoreapi.com/products`);
+        response.ok ? console.log('Successfull') : console.log('Failed');
+        const data = await response.json()
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+// ** generate the menu catagory
+
+const catagoryDisplay = async ()=>{
+
+    // ** Where to display
+    const listProducts = document.getElementById('unordered-list');
+
+    const data = await loadAllProducts();
+    
+    const productCategories = data.map(product => {
+        const {category} = product
+        
+        return category
+    });
+
+    const productCategoryUnique = new Set(productCategories);
+
+    const menuProducts = Array.from(productCategoryUnique);
+
+    menuProducts.forEach(product => {
+        const menuItem = document.createElement('li');
+        menuItem.classList.add('list-group-item')
+        menuItem.innerText = `
+        ${product}
+        `
+        listProducts.appendChild(menuItem);
+    })
+    
+};
+
+catagoryDisplay();
+
+
+// ** Search products
+
+document.getElementById('search-input').addEventListener('keypress', async(event)=>{
+    if (event.key === 'Enter') {
+        
+    }
+})
